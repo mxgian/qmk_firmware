43e037826fb95721095cc030990dc0571cb44f08
minhg <minh@minhgiang.com>
2022-12-29 21:59:51 -0500

    embrace

--- keyboards/thevankeyboards/embrace/keymaps/minhg/keymap.c
+++ keyboards/thevankeyboards/embrace/keymaps/minhg/keymap.c
@@ -1,4 +1,8 @@
 #include "embrace.h"
+#define _QW 0
+#define _L1 1
+#define _L2 2
+#define _L3 3
 enum custom_keycodes {
   M_IME = SAFE_RANGE
 };
@@ -22,22 +26,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_MUTE,
-               KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-               LT(1, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, LT(1, KC_QUOT),
-               KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LT(2, KC_ENT),
-               KC_LCTL, MO(2), KC_LGUI, KC_ENT, KC_SPC, KC_RALT, KC_RSFT, KC_RCTL, TG(3)),
-  [1] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-               KC_TRNS, KC_BSLS, KC_QUOT, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_DOWN, KC_UP, KC_LEFT, KC_RIGHT, KC_TRNS,
-                KC_TRNS, KC_ESC, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP, KC_MPLY, KC_MPRV, KC_MNXT, KC_TRNS,
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET),
-  [2] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TILD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS, KC_ESC, KC_PIPE, KC_DQT, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_4, KC_5, KC_6, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_1, KC_2, KC_3, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-  [3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_TRNS, KC_ESC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_TRNS, KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_LALT, KC_TRNS, KC_SPC, KC_C, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+               KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+               LT(_L3, KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, LT(1, KC_ENT),
+               KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+               KC_LCTL, KC_LALT, KC_LGUI,  LT(_L1,KC_BSPC),  LT(_L2,KC_SPC),   LT(_L3,KC_LEFT),  LT(_L1,KC_DOWN), LT(_L2,KC_UP), LT(_L3,KC_RIGHT)),
+  [1] = LAYOUT( KC_1, 	 KC_2,    KC_3,    KC_4,    KC_5,    KC_6, KC_TRNS,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DQUO,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_MSTP, KC_MPLY, KC_MPRV, KC_MNXT, KC_RSFT,
+    _______, KC_LGUI, _______,          _______, _______,          _______, _______, _______, _______),
+  [2] = LAYOUT( KC_1, 	 KC_2,    KC_3,    KC_4,    KC_5,    KC_6, KC_TRNS,
+    _______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
+    _______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_VOLD, _______,
+    _______, _______, _______,          _______, _______,          _______, _______, _______, _______
+    ),
+  [3] = LAYOUT(  KC_F2, 	 KC_F10,    KC_F12,    RGB_MOD,    RGB_VAI,  RGB_VAD, RGB_TOG,
+    _______, _______, KC_UP, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_DEL,
+    _______,  KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_VOLU,
+    KC_LSFT, RESET,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_F9,   KC_F10,  KC_F11,  KC_UP,  KC_VOLD,
+    _______, KC_LSFT, KC_B,            _______,  _______,             _______, KC_LEFT, KC_DOWN, KC_RIGHT
+    )
 };
 
 
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) {
       if (clockwise) {
         tap_code(KC_VOLU);
@@ -45,4 +59,5 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         tap_code(KC_VOLD);
       }
 }
+return true;
 }
